4da4741282c0f86bd2a7a3044eea6c313d22d060
hiroxto <hiroxto@gmail.com>
2021-08-11 17:58:29 +0900

    仕様の内容を変更
    Toneの定義を分かりやすく書き直し
    実際に鳴る音について削除

--- components/pages/cl-sound/Specification.vue
+++ components/pages/cl-sound/Specification.vue
@@ -3,8 +3,8 @@
     <h3>仕様</h3>
     <div class="description">
       <p>EMVCoが出しているContactless Specifications for Payment Systemsという仕様書にEMV Contactlessの仕様全般が書かれていて，オーディオ関連は9.1.2 Audio Indicationに書かれている．</p>
-      <p>普段聴く音はSuccess Toneと定義されていて，約500msの周期で約1500Hzの正弦波と規定されている．</p>
-      <p>ただし，実装する場合はリーダは意図した動作環境ではっきりと聞こえるレベルでトーンを鳴らす必要があるため，必ずしも仕様書通りの信号がリーダから鳴るとは限らない．</p>
+      <p>読み取り完了音はSuccess Toneと定義されていて，約1500Hzの正弦波を約500ミリ秒の周期で鳴らすと規定されている．</p>
+      <p>警告音はAlert Toneと定義されていて，約750Hzの正弦波を使用して，約200ミリ秒オン，200ミリ秒オフ，約200ミリ秒オンのダブルビープを鳴らすと規定されている．</p>
     </div>
   </div>
 </template>
