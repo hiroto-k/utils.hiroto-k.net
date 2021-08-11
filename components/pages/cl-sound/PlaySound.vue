8332b42db14f45c38743681264b6eec086463a78
hiroxto <hiroxto@gmail.com>
2021-08-11 17:52:58 +0900

    Alert Toneの再生を実装

--- components/pages/cl-sound/PlaySound.vue
+++ components/pages/cl-sound/PlaySound.vue
@@ -10,6 +10,16 @@
       >
         Play Success Tone
       </b-button>
+
+      <b-button
+        size="is-medium"
+        type="is-danger"
+        icon-left="contactless-payment"
+        rounded
+        @click="playAlertTone"
+      >
+        Play Alert Tone
+      </b-button>
     </div>
   </div>
 </template>
@@ -31,9 +41,30 @@ export default defineComponent({
       osc.stop(0.5);
     };
 
+    const playAlert = (ctx: AudioContext, start: number, stop: number): void => {
+      const osc = ctx.createOscillator();
+      osc.type = 'sine';
+      osc.frequency.value = 750;
+      osc.connect(ctx.destination);
+      osc.start(start);
+      osc.stop(stop);
+    };
+    const playEmpty = (ctx: AudioContext, start: number, stop: number): void => {
+      const emptySource = ctx.createBufferSource();
+      emptySource.start(start);
+      emptySource.stop(stop);
+    };
+    const playAlertTone = (): void => {
+      const ctx = new AudioContext();
+      playAlert(ctx, 0, 0.2);
+      playEmpty(ctx, 0.2, 0.4);
+      playAlert(ctx, 0.4, 0.6);
+    };
+
     return {
       audioContext,
       playSuccessTone,
+      playAlertTone,
     };
   },
 });
