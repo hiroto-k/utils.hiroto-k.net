b2888a903d2985ab27b060d6eaffa54bc41448b4
hiroxto <hiroxto@gmail.com>
2021-09-25 18:07:45 +0900

    2020年改正のページを作成

---
+++ pages/train-number/2020-03-14.vue
@@ -0,0 +1,29 @@
+<template>
+  <train-number-page
+    :title="title"
+    :source="source"
+  >
+  </train-number-page>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from '@nuxtjs/composition-api';
+import TrainNumberPage from '~/components/pages/train-number/TrainNumberPage';
+import contentSource from '~/assets/train-number/2020-03-14.md';
+
+export default defineComponent({
+  name: 'TrainNumber20200314Page',
+  components: {
+    TrainNumberPage,
+  },
+  setup () {
+    const title = ref<string>('2020年3月14日 改正');
+    const source = computed<string>(() => contentSource);
+
+    return {
+      title,
+      source,
+    };
+  },
+});
+</script>
