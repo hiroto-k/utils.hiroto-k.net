ad12fe54b45403ab7ea7c6bf24cf70b6752a00ae
hiroxto <hiroxto@gmail.com>
2021-09-25 18:08:17 +0900

    コンポーネント名を修正

--- pages/train-number/2019-03-16.vue
+++ pages/train-number/2019-03-16.vue
@@ -12,7 +12,7 @@ import TrainNumberPage from '~/components/pages/train-number/TrainNumberPage';
 import contentSource from '~/assets/train-number/2019-03-16.md';
 
 export default defineComponent({
-  name: 'TrainNumber20180317Page',
+  name: 'TrainNumber20190316Page',
   components: {
     TrainNumberPage,
   },
