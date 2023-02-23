40b5247e055e45740804b73eaa67fbf3f467ca74
hiroxto <hiroxto@gmail.com>
2023-02-23 17:14:47 +0900

    暫定的な経路入力画面を作成

--- pages/fare-ticket-route/index.vue
+++ pages/fare-ticket-route/index.vue
@@ -51,10 +51,55 @@
         </div>
       </div>
 
-      <div class="content routes">
+      <div class="content routes block">
         <h3>
           経路
         </h3>
+        <div class="buttons">
+          <b-button @click="addRoute" type="is-info">
+            追加
+          </b-button>
+          <b-button @click="removeEmptyRoutes" type="is-danger is-light">
+            空の経路を削除
+          </b-button>
+          <b-button @click="removeAllRoutes" type="is-danger">
+            全ての経路を削除
+          </b-button>
+        </div>
+
+        <div
+          v-for="(route, routeIndex) in routes"
+          :key="routeIndex"
+          class="columns"
+        >
+          <div class="column is-1">
+            <p>
+              路線数: {{ routeIndex + 1 }}
+            </p>
+          </div>
+          <div class="column">
+            <b-field label="路線名">
+              <b-input
+                v-model="route.line"
+                type="text"
+                placeholder="路線名"
+                @keydown.tab.native="onKeyupTab(routeIndex)"
+              ></b-input>
+            </b-field>
+          </div>
+          <div class="column">
+            <b-field label="接続駅">
+              <b-input
+                v-model="route.station"
+                type="text"
+                placeholder="接続駅"
+              ></b-input>
+            </b-field>
+          </div>
+          <div class="column is-1">
+            <button class="delete" tabindex="-1"></button>
+          </div>
+        </div>
       </div>
 
       <div class="content routes">
@@ -70,6 +115,11 @@
 <script lang="ts">
 import { computed, defineComponent, ref, useMeta } from '@nuxtjs/composition-api';
 
+interface Route {
+  line: string;
+  station: string;
+}
+
 export default defineComponent({
   head: {},
   setup () {
@@ -79,6 +129,23 @@ export default defineComponent({
     const date = ref<string>('');
     const departure = ref<string>('');
     const destination = ref<string>('');
+    const createRoute = (): Route => {
+      return { line: '', station: '' };
+    };
+    const routes = ref<Route[]>([createRoute()]);
+    const addRoute = () => routes.value.push(createRoute());
+    const removeEmptyRoutes = () => {
+      const newRoutes = routes.value.filter(route => {
+        return route.line.trim() !== '' || route.station.trim() !== '';
+      });
+      routes.value = newRoutes.length === 0 ? [createRoute()] : newRoutes;
+    };
+    const removeAllRoutes = () => (routes.value = [createRoute()]);
+    const onKeyupTab = (index: number) => {
+      if (routes.value.length - 1 === index) {
+        addRoute();
+      }
+    };
     const output = computed<string>(() => {
       return `券種: ${type.value}\n\n利用開始日: ${date.value}\n\n区間: ${departure.value}→${destination.value}\n\n`;
     });
@@ -97,6 +164,12 @@ export default defineComponent({
       date,
       departure,
       destination,
+      createRoute,
+      routes,
+      addRoute,
+      removeEmptyRoutes,
+      removeAllRoutes,
+      onKeyupTab,
       output,
     };
   },
