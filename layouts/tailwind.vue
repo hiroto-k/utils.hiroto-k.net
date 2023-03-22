e691744531450304f6f3442220fa18f6d45ee562
hiroxto <hiroxto@gmail.com>
2023-03-22 21:20:03 +0900

    tailwindのレイアウトファイルを作成

---
+++ layouts/tailwind.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <main>
+      <nuxt>
+      </nuxt>
+    </main>
+    <page-footer>
+    </page-footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  components: {
+  },
+});
+</script>
+
+<style scoped>
+.app {
+  font-family: "Noto Sans JP", "Helvetica", sans-serif;
+}
+</style>
