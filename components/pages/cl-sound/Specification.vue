3e97bddc0d8e14a047d05fbff7e292a052f962d7
hiroxto <hiroxto@gmail.com>
2023-03-22 23:54:55 +0900

    CLサウンドのページをTailwind仕様に変更

--- components/pages/cl-sound/Specification.vue
+++ components/pages/cl-sound/Specification.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="specification mt-5">
-    <h3>仕様</h3>
+    <h3 class="text-3xl font-semibold mb-2">
+      仕様
+    </h3>
     <div class="description">
-      <p>EMVCoが出しているContactless Specifications for Payment Systemsという仕様書にEMV Contactlessの仕様全般が書かれていて，オーディオ関連は9.1.2 Audio Indicationに書かれている．</p>
-      <p>読み取り完了音はSuccess Toneと定義されていて，約1500Hzの正弦波を約500ミリ秒の周期で鳴らすと規定されている．</p>
-      <p>警告音はAlert Toneと定義されていて，約750Hzの正弦波を使用して，約200ミリ秒オン，200ミリ秒オフ，約200ミリ秒オンのダブルビープを鳴らすと規定されている．</p>
+      <p class="text-lg">
+        EMVCoが出しているContactless Specifications for Payment Systemsという仕様書にEMV Contactlessの仕様全般が書かれていて，オーディオ関連は9.1.2 Audio Indicationに書かれている．
+      </p>
+      <p class="text-lg">
+        読み取り完了音はSuccess Toneと定義されていて，約1500Hzの正弦波を約500ミリ秒の周期で鳴らすと規定されている．
+      </p>
+      <p class="text-lg">
+        警告音はAlert Toneと定義されていて，約750Hzの正弦波を使用して，約200ミリ秒オン，200ミリ秒オフ，約200ミリ秒オンのダブルビープを鳴らすと規定されている．
+      </p>
     </div>
   </div>
 </template>
--- pages/cl-sound/index.vue
+++ pages/cl-sound/index.vue
@@ -1,21 +1,19 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <h1 class="title" v-text="title">
-      </h1>
+  <div class="container">
+    <h1 class="title" v-text="title">
+    </h1>
 
-      <h2 class="subtitle" v-text="description">
-      </h2>
+    <p class="subtitle" v-text="description">
+    </p>
 
-      <div class="content">
-        <play-sound>
-        </play-sound>
+    <div class="content">
+      <play-sound>
+      </play-sound>
 
-        <specification>
-        </specification>
-      </div>
+      <specification>
+      </specification>
     </div>
-  </section>
+  </div>
 </template>
 
 <script lang="ts">
@@ -24,6 +22,7 @@ import PlaySound from '~/components/pages/cl-sound/PlaySound';
 import Specification from '~/components/pages/cl-sound/Specification';
 
 export default defineComponent({
+  layout: 'tailwind',
   head: {},
   setup () {
     const title = ref<string>('EMVコンタクトレスのサウンドをWeb Audio APIで再生');
