285952c3dfd824309763a4b3e50f2ee7dda70416
hiroxto <hiroxto@gmail.com>
2023-03-23 02:22:42 +0900

    ページフッターをTailwind仕様のページフッターを作成

--- layouts/tailwind.vue
+++ layouts/tailwind.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="app bg-white scroll-smooth h-screen p-10">
-    <main>
+  <div class="app bg-white scroll-smooth h-screen">
+    <main class="p-10">
       <nuxt>
       </nuxt>
     </main>
+
+    <tailwind-page-footer>
+    </tailwind-page-footer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api';
+import TailwindPageFooter from '~/components/layouts/TailwindPageFooter';
 
 export default defineComponent({
   components: {
+    TailwindPageFooter,
   },
 });
 </script>
