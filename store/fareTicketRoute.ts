553ba06adeb8e3b4ecfce100de5d181504a3bbdb
hiroxto <hiroxto@gmail.com>
2023-03-27 16:27:26 +0900

    乗車券の経路作成のstoreを作成

---
+++ store/fareTicketRoute.ts
@@ -0,0 +1,66 @@
+import { defineStore } from 'pinia';
+import { FareTicketRouteStore, TicketType } from '../types';
+
+export const useQrCodeGeneratorStore = defineStore<'FareTicketRoute', FareTicketRouteStore>('FareTicketRoute', {
+  state: () => ({
+    type: '片道乗車券',
+    month: '',
+    day: '',
+    skipDate: false,
+    routes: [],
+    routes1: [],
+    routes2: [],
+    departure: '',
+    via: '',
+    destination: '',
+  }),
+  actions: {
+    setType (type: TicketType): void {
+      this.type = type;
+    },
+    resetType (): void {
+      this.type = '片道乗車券';
+    },
+    setMonth (month: string): void {
+      this.month = month;
+    },
+    setDay (day: string): void {
+      this.day = day;
+    },
+    unUseDate (): void {
+      this.skipDate = true;
+      this.month = '省略';
+      this.day = '省略';
+    },
+    useDate (): void {
+      this.skipDate = false;
+      this.month = '';
+      this.day = '';
+    },
+    setDate (addDate: number) {
+      const today = new Date();
+      today.setDate(today.getDate() + addDate);
+      this.month = (today.getMonth() + 1).toString();
+      this.day = today.getDate().toString();
+    },
+    setUndefinedDate (): void {
+      const undefinedDate = '     ';
+      this.month = undefinedDate;
+      this.day = undefinedDate;
+    },
+    setDeparture (departure: string): void {
+      this.departure = departure;
+    },
+    setVia (via: string): void {
+      this.via = via;
+    },
+    setDestination (destination: string): void {
+      this.destination = destination;
+    },
+    resetStations (): void {
+      this.departure = '';
+      this.via = '';
+      this.destination = '';
+    },
+  },
+});
--- types/state.d.ts
+++ types/state.d.ts
@@ -1,4 +1,17 @@
-import { QrCodeErrorCorrectionLevel, QrCodeRenderAsOptionValue } from './index';
+import { QrCodeErrorCorrectionLevel, QrCodeRenderAsOptionValue, Route, TicketType } from './index';
+
+export interface FareTicketRouteStore {
+  type: TicketType
+  month: string
+  day: string
+  skipDate: boolean
+  routes: Route[],
+  routes1: Route[],
+  routes2: Route[],
+  departure: string
+  via: string
+  destination: string
+}
 
 export interface QrCodeGeneratorState {
   value: string;
