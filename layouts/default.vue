172f72c6a6116cc068817fd8351fde977eb26617
Hiroto Kitazawa <hiroxto@gmail.com>
2023-06-04 19:00:01 +0900

    デフォルトのheadの定義場所をテンプレートに変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,5 +17,19 @@ export default defineNuxtComponent({
   components: {
     PageFooter,
   },
+  setup () {
+    useHead({
+      titleTemplate: '%s | utils.hiroxto.net',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Generic description.' },
+      ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: '192x192', href: '/apple-touch-icon.png' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap' },
+      ],
+    });
+  },
 });
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,6 @@
 
 export default defineNuxtConfig({
   /*
-  ** Headers of the page
-  */
-  // head: {
-  //   titleTemplate: '%s | utils.hiroxto.net',
-  //   meta: [
-  //     { charset: 'utf-8' },
-  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-  //     { hid: 'description', name: 'description', content: 'Generic description.' },
-  //   ],
-  //   link: [
-  //     { rel: 'apple-touch-icon', sizes: '192x192', href: '/apple-touch-icon.png' },
-  //     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap' },
-  //   ],
-  // },
-
-  /*
   ** Global CSS
   */
   css: [
