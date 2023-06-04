bca7b5326ad852ab75d9ddcc852af8289f877b87
Hiroto Kitazawa <hiroxto@gmail.com>
2023-06-04 22:04:38 +0900

    列番計算のコンポーネントのvee-validate周りをv4仕様に更新

--- components/pages/train-number-calc/NumberCalc.vue
+++ components/pages/train-number-calc/NumberCalc.vue
@@ -1,26 +1,21 @@
 <template>
   <div>
     <div class="mt-5">
-      <validation-provider
-        name="列車番号"
-        rules="numeric|between:1,9999|not-starts-with-zero"
-        v-slot="{ errors }"
+      <span class="block text-lg font-semibold text-slate-700">
+        列車番号
+      </span>
+      <input
+        @input="handleChange"
+        v-model="value"
+        type="text"
+        class="mt-1 px-3 py-2 bg-white shadow-sm border border-2 placeholder-slate-400 focus:outline-none block w-full rounded-md"
+        :class="inputClass"
+        placeholder="列車番号を入力"
       >
-        <span class="block text-lg font-semibold text-slate-700">
-          列車番号
-        </span>
-        <input
-          v-model="trainNumber"
-          type="text"
-          class="mt-1 px-3 py-2 bg-white shadow-sm border border-2 placeholder-slate-400 focus:outline-none block w-full rounded-md"
-          :class="getInputClass(errors)"
-          placeholder="列車番号を入力"
-        >
 
-        <p class="text-red-500">
-          {{ errors[0] }}
-        </p>
-      </validation-provider>
+      <p class="text-red-500" v-if="errorMessage != null">
+        {{ errorMessage }}
+      </p>
     </div>
 
     <button
@@ -33,48 +28,40 @@
 </template>
 
 <script lang="ts">
-import { ValidationProvider, extend, localize } from 'vee-validate';
-import { numeric, between } from 'vee-validate/dist/rules';
-import ja from 'vee-validate/dist/locale/ja.json';
+import { useField, defineRule } from 'vee-validate';
+import { numeric, between } from '@vee-validate/rules';
 import TrainNumberCalc from '~/lib/TrainNumberCalc';
 import { TrainNumberType } from '~/types';
 
-ja.messages['not-starts-with-zero'] = '{_field_}は不正な値です。';
-extend('numeric', numeric);
-extend('between', between);
-extend('not-starts-with-zero', {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  validate: (value: any): boolean => {
-    if (typeof value === 'string') {
-      return !value.startsWith('0');
-    } else {
-      return false;
-    }
-  },
-});
-localize('ja', {
-  messages: ja.messages,
-});
+defineRule('numeric', numeric);
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+defineRule('between', between);
+defineRule('not-starts-with-zero', (value) => typeof value === 'string' ? !value.startsWith('0') : false);
+
+// ja.messages['not-starts-with-zero'] = '{_field_}は不正な値です。';
+// localize('ja', {
+//   messages: ja.messages,
+// });
 
 export default defineComponent({
   name: 'NumberCalc',
   setup () {
-    const trainNumber = ref('');
-    const numberCalc = computed<TrainNumberCalc>(() => new TrainNumberCalc(trainNumber.value));
+    const { value, errorMessage, handleChange } = useField('列車番号', 'numeric|between:1,9999|not-starts-with-zero', { initialValue: '' });
+    const numberCalc = computed<TrainNumberCalc>(() => new TrainNumberCalc(value.value));
     const trainType = computed<TrainNumberType|null>(() => numberCalc.value.calc());
     const isRenderTrainType = computed<boolean>(() => (trainType.value !== null));
-    const getInputClass = (errors: string[]): string => errors.length === 0 ? 'border-green-300 focus:border-green-500' : 'border-red-300 focus:border-red-500';
+    const inputClass = computed<string>(() => errorMessage.value == null ? 'border-green-300 focus:border-green-500' : 'border-red-300 focus:border-red-500');
 
     return {
-      trainNumber,
       numberCalc,
       trainType,
       isRenderTrainType,
-      getInputClass,
+      inputClass,
+      value,
+      errorMessage,
+      handleChange,
     };
-  },
-  components: {
-    ValidationProvider,
   },
 });
 </script>
