7011f5463891e65bb44e58ec612c7f12d12d8fee
Hiroto Kitazawa <hiroxto@gmail.com>
2024-08-25 23:37:55 +0900

    feat: headにattributeを追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,6 +20,10 @@ export default defineNuxtComponent({
   setup () {
     useHead({
       titleTemplate: '%s | utils.hiroxto.net',
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: https://ogp.me/ns#',
+      },
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
